bbff6500661fd8a02e6ddca2e1ac6f345154dd08
Frederic Duby <fcd1@columbia.edu>
2015-02-08 10:47:59 -0500

    Add items/show.php to replace ignored exhibit-builder/exhibits/item.php In previous versions of ExhibitBuilder, exhibit-builder/exhibits/item.php was used in the view for the item page via an exhibit. However, in ExhibitBuilder 3.1.3, this file seems to be ignored. However, if it is renamed and placed in items/ , it is then used by the view.

---
+++ items/show.php
@@ -0,0 +1,71 @@
+      <?php
+        echo head(array('title' => metadata('item', array('Dublin Core', 'Title')),
+			'bodyclass' => 'exhibits exhibit-item-show'));
+      ?>
+      <div id="item-page-content">
+        <div id="primary" class="show">
+          <h3 class="exhibit-title-link">Exhibition: 
+          <?php echo exhibit_builder_link_to_exhibit(null,null,array('class' => 'link-to-exhibit')); ?>
+          </h3>
+          <table>
+            <tr>
+              <td>
+                <?php
+                  // fcd1, 01/31/14:
+                  // Retrieve, in an array, the list of exhibit pages containing the current item.
+                  $exhibit_pages = CulCustomizePlugin::return_exhibit_pages_containing_current_item();
+
+                  // fcd1, 01/31/14
+                  // Print out a link back to the exhibit page we came from.
+                  // The second parameter is set to TRUE to indicate this is a legacy exhibition
+                  $exhibit_page_containing_item = 
+                    CulCustomizePlugin::return_exhibit_page_to_link_back_to($exhibit_pages, TRUE);
+                  if ($exhibit_page_containing_item) {
+                    $link_title = $exhibit->title.': ';
+                    $parent_page = $exhibit_page_containing_item->getParent();
+                    $link_title .= ( $parent_page ? $parent_page->title . ': ' : '');
+                    $link_title .= $exhibit_page_containing_item->title.'</a></p>';
+                    echo '<h3>View item in context</h3>';
+                    echo '<p><a href="'.
+                      html_escape(exhibit_builder_exhibit_uri($exhibit, $exhibit_page_containing_item)).
+                      '">'.$link_title.'</a></p>';
+                  }
+
+                  // fcd1, 01/31/14:
+                  // Print out a list of other exhibit pages containing the item
+                  // CulCustomizePlugin::display_links_to_exhibit_pages_containing_item takes
+                  // care of filtering out $exhibit_page_containing_item, since we already
+                  // displayed a link to that page
+                  if(!empty($exhibit_pages)) {
+                    CulCustomizePlugin::display_links_to_exhibit_pages_containing_item(
+										       $exhibit_pages,
+										       $exhibit_page_containing_item);
+                  }
+                ?>
+                <h1 class="item-title">Item Information</h1>
+                <div id="itemfiles">
+                  <?php 
+                    // fcd1, 12/22/14: Following is defined in plugins/CulCustomized/CulCustomizePlugin
+                    // All this method does is call files_for_item with hard-coded arguments.
+                    cul_files_for_item();
+                  ?>
+                </div>
+                <?php echo all_element_texts('item'); ?>
+                <?php if (metadata('item', 'has tags')): ?>
+                  <div class="tags">
+                    <h3><?php echo __('Tags'); ?></h3>
+                    <?php echo tag_string('item'); ?>
+                  </div>
+                <?php endif;?>
+                <div id="citation" class="field">
+                  <h3><?php echo __('Citation'); ?></h3>
+                  <p id="citation-value" class="field-value">
+                    <?php echo metadata('item', 'citation', array('no_escape' => true)); ?>
+                  </p>
+                </div>
+              </td>
+            </tr>
+          </table>
+        </div><!--end id="primary"-->
+      </div><!--end id="item-page-content"-->
+      <?php echo foot(); ?>
